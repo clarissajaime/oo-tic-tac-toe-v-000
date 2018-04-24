0703d007ff9b4135d0d6fc61fe2dc882f3d1a9e0
Clarissa Jaime <clarissanjaime@gmail.com>
2018-04-24 23:50:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,7 +1,7 @@
 class TicTacToe
-  WIN_COMBINATIONS = [
-  [0,1,2], # Top row
-  [3,4,5]  # Middle row
-  # et cetera, creating a nested array for each win combination
-]
+    WIN_COMBINATIONS = [
+    [0,1,2], # Top row
+    [3,4,5]  # Middle row
+    # et cetera, creating a nested array for each win combination
+  ]
 end 
